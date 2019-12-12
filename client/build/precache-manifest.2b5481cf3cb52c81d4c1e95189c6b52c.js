b6d7c9257eb93a0c4f3c6f2ae65a3a5f6eed90d5
Apurv Deshpande <apurvdeshpande02@gmail.com>
2019-12-12 17:16:05 +0530

    New changes

---
+++ client/build/precache-manifest.2b5481cf3cb52c81d4c1e95189c6b52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75b808eefb95dd8bde7dd0c60fe8344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c65ead1c5958660498a0",
+    "url": "/static/css/main.8dfadfe2.chunk.css"
+  },
+  {
+    "revision": "c7e60932295b29882732",
+    "url": "/static/js/2.b9a79cec.chunk.js"
+  },
+  {
+    "revision": "c65ead1c5958660498a0",
+    "url": "/static/js/main.0c97f46e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2acd33aa989dda73ae0b879e2f2c3c46",
+    "url": "/static/media/showcase.2acd33aa.jpg"
+  }
+]);
